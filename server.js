2ba0437042dbf51eb2ce372e7927a8a4c531aec2
Nathan Plouvier <nathanplouvier60@gmail.com>
2019-11-13 17:37:11 +0100

    wip

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-
+const apiRoutes = require('./api/index');
 const app = express()
+
 
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
@@ -11,6 +12,8 @@ app.use('/', serveStatic(path.join(__dirname, '/dist')))
 app.get(/.*/, function (req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
+
+app.use('/api/v1', apiRoutes);
 
 const port = process.env.PORT || 8080
 app.listen(port)
